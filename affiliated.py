f43893d8f118e769267569d95d1b4df50d737ced
Miguel de Val-Borro <miguel@archlinux.net>
2017-10-24 18:53:03 -0400

    provisionally accepted packages

--- affiliated.py
+++ affiliated.py
@@ -9,12 +9,17 @@ package_list = sorted(data['packages'], key=lambda k: k['name'].lower())
 
 stable_dict = {False: 'No', True: 'Yes'}
 
-with open("registry.tex", "w") as f:
+row = ('\\href{{{0}}}{{{1}}} & {2} & '
+      '\\href{{https://pypi.python.org/pypi/{3}}}{{{3}}} & {4} \\\\\n')
+
+with open("registry.tex", "w") as f1, open("registry_prov.tex", "w") as f2:
     for item in package_list:
         if not item['provisional']:
-            f.write('\\href{{{0}}}{{{1}}} & {2} & '
-                    '\\href{{https://pypi.python.org/pypi/{3}}}{{{3}}} & {4} '
-                    '\\\\\n'.format(item["repo_url"], item["name"],
-                                    stable_dict[item["stable"]],
-                                    item['pypi_name'],
-                                    re.sub(' <.*?>', '', item["maintainer"])))
+            f1.write(row.format(item["repo_url"], item["name"],
+                                stable_dict[item["stable"]], item['pypi_name'],
+                                re.sub(' <.*?>', '', item["maintainer"])))
+        else:
+            f2.write(row.format(item["repo_url"], 
+                                re.sub('_', '\_', item["name"]),
+                                stable_dict[item["stable"]], item['pypi_name'],
+                                re.sub(' <.*?>', '', item["maintainer"])))
