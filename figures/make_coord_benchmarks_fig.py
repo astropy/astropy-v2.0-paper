66aac618daac6794c1ac2773c3f3aecc09ffebc2
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-12-11 13:01:33 -0500

    only show max diff

--- figures/make_coord_benchmarks_fig.py
+++ figures/make_coord_benchmarks_fig.py
@@ -5,10 +5,12 @@ Originally contributed by @dpshelio
 import matplotlib.pyplot as plt
 import numpy as np
 
-cc = [[0, 0], [0, 0]]
-ap = [[0.218, 0.196], [0.249, 0.056]]  # astropy - palpy
-ae = [[0.459, 0.458], [0.860, 0.231]]  # astropy - pyephem
-pe = [[0.563, 0.570], [1.012, 0.253]]  # palpy - pyephem
+# max difference to 3 significant figures:
+cc = 0
+ap = 0.249 # astropy - palpy
+ae = 0.860 # astropy - pyephem
+pe = 1.012 # palpy - pyephem
+
 data = {'astropy-astropy':   cc,
         'astropy-kapteyn':   cc, 'kapteyn-astropy':  cc,
         'astropy-pytpm':     cc, 'pytpm-astropy':    cc,
@@ -42,29 +44,28 @@ names = ['astropy', 'kapteyn', 'pytpm', 'palpy', 'pyast', 'pyslalib', 'pyephem']
 n = len(names)
 
 # Fill a single 2D array with the data to visualize
-M = np.zeros((2*n, 2*n))
+M = np.zeros((n, n))
 for i in range(n):
     for j in range(n):
-        if 2*i >= 2*j:
-            M[2*j:2*j+2, 2*i:2*i+2] = np.nan
+        if i >= j:
+            M[j:j+1, i:i+1] = np.nan
         else:
-            M[2*j:2*j+2, 2*i:2*i+2] = np.array(data["{}-{}".format(names[j],
-                                                                   names[i])])
+            M[j:j+1, i:i+1] = data["{}-{}".format(names[j], names[i])]
 
-
+# Make the figure
 fig, ax = plt.subplots(1, 1, figsize=(6,6))
 
 c = ax.imshow(M, cmap='YlOrBr', zorder=1)
 
-maj_ticks = np.arange(0, 2*n, 2) + 0.5
+maj_ticks = np.arange(0, n, 1)
 
 ax.set_xticks(maj_ticks)
 ax.set_yticks(maj_ticks)
 ax.set_xticklabels(names, fontsize=14)
 ax.set_yticklabels(names, fontsize=14)
 
-ax.set_xlim(-0.5, 2*n-2.5+0.01)
-ax.set_ylim(2*n-0.5, 1.5-0.01)
+ax.set_xlim(-0.5, n-1.5+0.01)
+ax.set_ylim(n-0.5, 0.5-0.01)
 
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
@@ -72,27 +73,18 @@ ax.spines['top'].set_visible(False)
 grid_style = dict(marker='', linewidth=1, zorder=8)
 
 # "minor" grid
-for i in range(0, 2*n):
-    ax.plot([-.5, np.floor(i+.5)-.5], [i+0.5, i+0.5], color='#aaaaaa',
+for i in range(0, n):
+    ax.plot([-.5, i+.5], [i+0.5, i+0.5], color='#aaaaaa',
             **grid_style)
-    if i < 2*n-1:
-        ax.plot([i+0.5, i+0.5], [np.ceil(i+.5)+.5, 2*n], color='#aaaaaa',
-                **grid_style)
-
-# "major" grid
-for i in range(-1, 2*n+1, 2):
-    ax.plot([-.5, i+.5], [i+0.5, i+0.5], color='#666666',
+    ax.plot([i+0.5, i+0.5], [i+.5, n], color='#aaaaaa',
             **grid_style)
-    if i < 2*n-1:
-        ax.plot([i+0.5, i+0.5], [i+.5, 2*n], color='#666666',
-                **grid_style)
 
 cbaxes = fig.add_axes([0.2, 0.825, 0.7, 0.025])
 cb = fig.colorbar(cax=cbaxes, mappable=c, orientation='horizontal')
-cb.set_label('accuracy metric [arcsec]', labelpad=10, fontsize=20)
+cb.set_label('max. difference [arcsec]', labelpad=10, fontsize=20)
 cb.ax.xaxis.set_ticks_position('top')
 cb.ax.xaxis.set_label_position('top')
-[l.set_fontsize(14) for l in cb.ax.xaxis.get_ticklabels()]
+[l.set_fontsize(16) for l in cb.ax.xaxis.get_ticklabels()]
 cb.set_clim(0, 1)
 
 fig.subplots_adjust(left=0.2, bottom=0.02)
