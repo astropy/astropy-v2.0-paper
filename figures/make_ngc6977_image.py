94f831858e3d6ffc7e84c394701b9d3c83835a95
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-12-12 15:02:24 -0500

    fix orientation

--- figures/make_ngc6977_image.py
+++ figures/make_ngc6977_image.py
@@ -11,18 +11,22 @@ from reproject import reproject_interp
 import numpy as np
 
 # load the data
-g_name = get_pkg_data_filename('visualization/reprojected_sdss_g.fits.bz2')
-r_name = get_pkg_data_filename('visualization/reprojected_sdss_r.fits.bz2')
-i_name = get_pkg_data_filename('visualization/reprojected_sdss_i.fits.bz2')
+# g_name = get_pkg_data_filename('visualization/reprojected_sdss_g.fits.bz2')
+# r_name = get_pkg_data_filename('visualization/reprojected_sdss_r.fits.bz2')
+# i_name = get_pkg_data_filename('visualization/reprojected_sdss_i.fits.bz2')
+g_name = get_pkg_data_filename('hash/fb425fccf6326e34f4566711d8910364')
+r_name = get_pkg_data_filename('hash/e6670142bc3c83936d84bd5d5d331d6a')
+i_name = get_pkg_data_filename('hash/ed6e4146ea0a1d2af0f005dd8ba79282')
 
 g = fits.open(g_name)[0]
 r = fits.open(r_name)[0]
 i = fits.open(i_name)[0]
 
 # Rotate the images
-ang = np.deg2rad(90)
+ang = np.deg2rad(-90)
 wcs = WCS(g.header)
 wcs.wcs.cd = wcs.wcs.cd @ np.array([[np.cos(ang),-np.sin(ang)],[np.sin(ang),np.cos(ang)]])
+wcs.wcs.crpix = tuple([x/2. for x in g.data.shape])
 
 imgs = []
 for img in [g,r,i]:
@@ -34,7 +38,7 @@ for img in [g,r,i]:
     imgs.append(arr)
 
 # extract a smaller region around the Hickson 88 group
-slc = (slice(200, 580), slice(130, 550))
+slc = (slice(None, 400), slice(None))
 gp = imgs[0][slc]
 rp = imgs[1][slc]
 ip = imgs[2][slc]
@@ -43,16 +47,16 @@ ip = imgs[2][slc]
 rgb1 = make_lupton_rgb(ip, rp, gp)
 rgb2 = make_lupton_rgb(ip*0.8, rp, gp*1.3, Q=10, stretch=0.4)
 
-fig, axes = plt.subplots(1, 2, figsize=(10, 5.5),
+fig, axes = plt.subplots(1, 2, figsize=(10, 4),
                          sharex=True, sharey=True,
                          subplot_kw=dict(projection=wcs))
 
 axes[0].imshow(rgb1, origin='lower')
-axes[0].set_title('default parameters', fontsize=18)
+axes[0].set_title('default parameters', fontsize=14)
 axes[0].set_adjustable('box-forced')
 
 axes[1].imshow(rgb2, origin='lower')
-axes[1].set_title('Q=10, stretch=0.4', fontsize=18)
+axes[1].set_title('Q=10, stretch=0.4', fontsize=14)
 axes[1].set_adjustable('box-forced')
 
 for j in [0,1]:
@@ -61,8 +65,8 @@ for j in [0,1]:
 
     lon.set_axislabel('Right Ascension (ICRS)')
 
-    lon.set_ticks(spacing=1*u.arcmin)
-    lat.set_ticks(spacing=1*u.arcmin)
+    lon.set_ticks(spacing=2*u.arcmin)
+    lat.set_ticks(spacing=2*u.arcmin)
 
     lon.set_major_formatter('dd:mm')
     lat.set_major_formatter('dd:mm')
